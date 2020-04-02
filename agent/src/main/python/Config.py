755f4ff49aae409832a8d24c0ab016c95f77af6f
qianmoQ <shicheng@ttxit.com>
2020-04-02 15:11:40 +0800

    添加服务心跳上报json

--- agent/src/main/python/Config.py
+++ agent/src/main/python/Config.py
@@ -49,10 +49,43 @@ class ServiceCommandModel:
         self.id = ''  # 服务ID
         self.name = ''  # 服务名称
         self.type = ''  # 服务类型
-        self.commands = []  # 命令
+        self.commands = [ServiceCommandSimpleModel]  # 命令
 
     def print_json(self):
         print json.dumps(self.__dict__)
+
+
+class ServiceCommandSimpleModel:
+    """
+    服务命令实体
+    """
+
+    def __init__(self):
+        self.id = ''  # 服务ID
+        self.name = ''  # 服务名称
+        self.command = []  # 命令
+
+    def print_json(self):
+        print json.dumps(self.__dict__)
+
+
+def get_attr_by_key(content, key):
+    """
+    根据提供的key获取属性值
+    :param content: 源数据
+    :param key: key
+    :return: 属性值，默认为None
+    """
+    value = None
+    if key is '':
+        print '无效的主键'
+    else:
+        try:
+            value = content[key]
+        except Exception:
+            # print '无效的主键数据设置为None'
+            value = None
+    return value
 
 
 class ConfigUtils:
@@ -65,24 +98,6 @@ class ConfigUtils:
         self.content_command = yaml.load(file_command, Loader=yaml.FullLoader)
         file_command.close()
 
-    def get_attr_by_key(self, content, key):
-        """
-        根据提供的key获取属性值
-        :param content: 源数据
-        :param key: key
-        :return: 属性值，默认为None
-        """
-        value = None
-        if key is '':
-            print '无效的主键'
-        else:
-            try:
-                value = content[key]
-            except Exception:
-                # print '无效的主键数据设置为None'
-                value = None
-        return value
-
     def build_agent_configs(self):
         """
         解析探针配置文件处理监控数据
@@ -91,25 +106,25 @@ class ConfigUtils:
         models = []
         rules = self.content_agent['rules']
         for temp in rules:
-            rule = self.get_attr_by_key(content=rules, key=temp)
+            rule = get_attr_by_key(content=rules, key=temp)
             model = ConfigModel()
             model.id = temp
-            model.type = self.get_attr_by_key(content=rule, key='type')
-            model.name = self.get_attr_by_key(content=rule, key='name')
+            model.type = get_attr_by_key(content=rule, key='type')
+            model.name = get_attr_by_key(content=rule, key='name')
             # 解析主机信息部分
-            temp_hosts = self.get_attr_by_key(content=rule, key='hosts')
+            temp_hosts = get_attr_by_key(content=rule, key='hosts')
             hosts = []
             for h in temp_hosts:
-                host = self.get_attr_by_key(content=temp_hosts, key=h)
+                host = get_attr_by_key(content=temp_hosts, key=h)
                 model_host = ConfigHostModel()
                 model_host.id = h
-                model_host.host = self.get_attr_by_key(content=host, key='host')
-                model_host.username = self.get_attr_by_key(content=host, key='username')
-                model_host.password = self.get_attr_by_key(content=host, key='password')
-                model_host.command = self.get_attr_by_key(content=host, key='command')
+                model_host.host = get_attr_by_key(content=host, key='host')
+                model_host.username = get_attr_by_key(content=host, key='username')
+                model_host.password = get_attr_by_key(content=host, key='password')
+                model_host.command = get_attr_by_key(content=host, key='command')
                 hosts.append(model_host)
             model.hosts = hosts
-            model.message = self.get_attr_by_key(content=rule, key='message')
+            model.message = get_attr_by_key(content=rule, key='message')
             models.append(model)
         return models
 
@@ -122,36 +137,54 @@ class ConfigUtils:
         commands = self.content_command
         for command_temp in commands:
             service_command = ServiceCommandModel()
-            command = self.get_attr_by_key(content=commands, key=command_temp)
+            command = get_attr_by_key(content=commands, key=command_temp)
             service_command.id = command_temp
-            service_command.name = self.get_attr_by_key(content=command, key='name')
-            service_command.type = self.get_attr_by_key(content=command, key='type')
-            command_services = self.get_attr_by_key(content=command, key='commands')
+            service_command.name = get_attr_by_key(content=command, key='name')
+            service_command.type = get_attr_by_key(content=command, key='type')
+            command_services = get_attr_by_key(content=command, key='commands')
             command_temps = []
             if command_services is not None:
-                for command_service in self.get_attr_by_key(content=command, key='commands'):
-                    temp = self.get_attr_by_key(content=command_services, key=command_service)
-                    c = {}
-                    c['id'] = command_service
-                    c['name'] = self.get_attr_by_key(content=temp, key='name')
-                    c['command'] = self.get_attr_by_key(content=temp, key='command')
-                    print c
-                    command_temps.append(c)
+                for command_service in get_attr_by_key(content=command, key='commands'):
+                    temp = get_attr_by_key(content=command_services, key=command_service)
+                    command_simple = ServiceCommandSimpleModel()
+                    command_simple.id = command_service
+                    command_simple.name = get_attr_by_key(content=temp, key='name')
+                    command_simple.command = get_attr_by_key(content=temp, key='command')
+                    command_temps.append(command_simple)
             service_command.commands = command_temps
             models.append(service_command)
         return models
 
+    def get_command(self, service_name, service_type, cluster_name):
+        """
+        获取默认命名
+        :param service_name: 服务名称，例如：historical
+        :param service_type: 服务类型，例如：apache
+        :param cluster_name: 集群类型，例如：druid
+        :return: 命令想起
+        """
+        commands = self.build_command_configs()
+        command_response = None
+        for command in commands:
+            if command.name.strip() == cluster_name.strip() and command.type.strip() == service_type.strip():
+                if len(command.commands) > 0:
+                    for command_temp in command.commands:
+                        if command_temp.name.strip() == service_name.strip():
+                            command_response = command_temp
+        return command_response
+
 
 if __name__ == '__main__':
-    for model in ConfigUtils().build_command_configs():
-        print '-------------------华丽的分割线---------------------'
-        print
-        print model.id
-        print model.name
-        print model.type
-        print model.commands
-        print
-
-    for model in ConfigUtils().build_agent_configs():
-        for host in model.hosts:
-            host.print_json()
+    ConfigUtils().get_command(service_name='historical', service_type='io', cluster_name='druid')
+    # for model in ConfigUtils().build_command_configs():
+    #     print '-------------------华丽的分割线---------------------'
+    #     print
+    #     print model.id
+    #     print model.name
+    #     print model.type
+    #     print model.commands
+    #     print
+    #
+    # for model in ConfigUtils().build_agent_configs():
+    #     for host in model.hosts:
+    #         host.print_json()
--- agent/src/main/python/ImonitorService.py
+++ agent/src/main/python/ImonitorService.py
@@ -4,6 +4,10 @@
 # @Desc    : 服务器应用进程监控脚本
 # @File    : ImonitorService.py
 
+import calendar
+import json
+import time
+
 import RemoteCommand
 from Config import ConfigUtils
 
@@ -13,20 +17,38 @@ class MonitorService:
     def __init__(self):
         self.config = ConfigUtils().build_agent_configs()
 
-    def validate_service_status(self):
+    def service_info(self):
+        """
+        获取服务信息
+        :return: 服务信息
+        """
         for monitor in self.config:
-            print monitor.id
+            service_names = monitor.id.split('_')
             for host in monitor.hosts:
                 if host.command is None:
-                    host.command = 'ls -l'
-                buffer = RemoteCommand.command_ssh_remote(user=host.username,
-                                                          host=host.host,
-                                                          password=host.password,
-                                                          command=host.command)
-                print buffer.before
-                print '-------------------华丽的分割线---------------------'
-                print buffer.before.split()
+                    command = ConfigUtils().get_command(service_name=service_names[2], service_type=monitor.type,
+                                                        cluster_name=monitor.name)
+                    if command is not None:
+                        host.command = command.command
+                if host is not None and host.command is not None:
+                    buffer = RemoteCommand.command_ssh_remote(user=host.username,
+                                                              host=host.host,
+                                                              password=host.password,
+                                                              command=host.command)
+                    # print '-------------------华丽的分割线---------------------'
+                    service_heartbeat = buffer.before.split()
+                    heartbeat = {}
+                    heartbeat['service_name'] = service_names[2]
+                    heartbeat['service_username'] = service_heartbeat[0]
+                    heartbeat['service_pid'] = service_heartbeat[1]
+                    heartbeat['service_host'] = host.host
+                    heartbeat['service_timestamp'] = calendar.timegm(time.gmtime())
+                    service_status = 'DOWN'
+                    if service_heartbeat[1] > 0:
+                        service_status = 'UP'
+                    heartbeat['service_status'] = service_status
+                    print json.dumps(heartbeat)
 
 
 if __name__ == '__main__':
-    MonitorService().validate_service_status()
+    MonitorService().service_info()
