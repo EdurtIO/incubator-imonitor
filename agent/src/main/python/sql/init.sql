26da50ccf3674e53d6ca8d3dec201a91ce2c2667
qianmoQ <shicheng@ttxit.com>
2020-04-15 18:13:30 +0800

    完善部分README

--- agent/src/main/python/sql/init.sql
+++ agent/src/main/python/sql/init.sql
@@ -1,14 +1,3 @@
-CREATE TABLE `host` (
-    `host`            varchar(255),
-    `port`            varchar(255),
-    `username`        varchar(255),
-    `password`        varchar(255),
-    `command`         varchar(255),
-    `command_start`   varchar(255),
-    `command_stop`    varchar(255),
-    `command_restart` varchar(255)
-) COMMENT ='主机表';
-
 create table `monitor_memory_host_relation` (
     `host_id` int comment '主机ID',
     `monitor_memory_id` int comment '内存监控信息ID'
