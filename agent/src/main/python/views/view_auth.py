575af34d6c9540945c102c1aef5a270e8799265a
qianmoQ <shicheng@ttxit.com>
2020-04-20 14:26:02 +0800

    修复数据库不存在用户登录错误问题

--- agent/src/main/python/views/view_auth.py
+++ agent/src/main/python/views/view_auth.py
@@ -3,7 +3,7 @@
 # @Time    : 2020-04-08 16:40:20
 # @Desc    : 授权配置
 # @File    : view_auth.py
-from application_config import app
+from application_config import app, logger
 from application_config import login_manager
 from bin.assets import compile_auth_assets
 from db.model_logging_login import LoginLogging
@@ -48,7 +48,12 @@ def signin():
         flash('输入的密码错误')
         logging_login.status = False
         logging_login.reason = '输入的密码错误'
-        LoginLoggingService().add(model=logging_login, user_id=user.id)
+        # 匿名用户登录不做存储
+        try:
+            LoginLoggingService().add(model=logging_login, user_id=current_user.id)
+        except Exception as ex:
+            flash('无效的账号信息')
+            logger.error('not found <%s> user', login_form.email.data)
         return redirect(url_for('auth_view.signin'))
     return render_template('auth/signin.html', form=login_form, title='用户登录')
 
